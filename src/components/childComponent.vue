7781e7aba1a1dc0ab1b706338a3bc2ff66b2274a
romychvk <roman@zina.design>
2022-05-29 16:33:55 +0300

    Initial commit

---
+++ src/components/childComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="currentComponentFunction">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "ChildComponent",
+    data() {
+      return {
+        count: 1
+      }
+    },
+    methods: {
+      currentComponentFunction(){
+        this.$emit("customNamedEmit", "Button clicked " + this.count++ + " times");
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  button {
+    display: block;
+    font-size: 1.25rem;
+    padding: 5px 15px;
+    margin: 2rem auto;
+  }
+</style>
